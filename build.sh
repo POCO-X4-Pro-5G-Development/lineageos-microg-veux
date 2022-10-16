196b95d7e5ed2f05e651f72292bfbcd029dc4bba
SM9() <me@sm9.dev>
2022-10-16 13:25:50 +0100

    Simplify build script

--- build.sh
+++ build.sh
@@ -2,15 +2,11 @@
 
 set -e
 
-device="veux"
-branch="lineage-19.1"
-tag=$(date "+%G%m%d")
-
 # build
 docker pull lineageos4microg/docker-lineage-cicd
 docker run \
-    -e "BRANCH_NAME=$branch" \
-    -e "DEVICE_LIST=$device" \
+    -e "BRANCH_NAME=lineage-19.1" \
+    -e "DEVICE_LIST=veux" \
     -e "SIGN_BUILDS=true" \
     -e "SIGNATURE_SPOOFING=restricted" \
     -e "INCLUDE_PROPRIETARY=false" \
@@ -23,19 +19,3 @@ docker run \
     -v "$PWD/srv/keys:/srv/keys" \
     -v "$PWD/srv/local_manifests:/srv/local_manifests" \
    lineageos4microg/docker-lineage-cicd
-
-# create release
-files="$PWD/srv/zips/$device/$branch-$tag-UNOFFICIAL-$device*"
-sha256=$(cat "$PWD/srv/zips/$device/$branch-$tag-UNOFFICIAL-$device.zip.sha256sum" | grep -oP "^([a-f0-9]+)")
-
-gh release create \
-    --title "$branch-$tag-UNOFFICIAL-$device" \
-    --notes "sha256: \`$sha256\`" \
-    "$tag" \
-    $files
-
-# upload artifacts
-for file in $files;
-do
-    rclone copy "$file" ota:/ --progress
-done
